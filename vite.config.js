4a25f1f2e8757a2c70cf9d3f442a76e7556d153d
szho0086 <szho0086@student.monash.edu>
2024-10-17 01:18:45 +1100

    Trigger deployment

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 //   : '/'
 // })
 export default defineConfig({
-  base: '',
+  base: process.env.NODE_ENV === 'production'
+  ? '/Monash-FIT5032-LabProject/'
+  : '/',
   build: {
     outDir: 'dist'
   },
